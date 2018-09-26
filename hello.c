b222bf13ba8bbb117ee19f7ddc3b83b3fe65afcb
sadhanachallagi2000 <43573476+sadhanachallagi2000@users.noreply.github.com>
2018-09-26 18:34:42 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -5,3 +5,22 @@ int main()
    printf("test");
    return 0;
 }
+what is #
+ Preprocessor Directive 
+ what is <stdio.h>
+  standard input/output header (<stdio.h>)
+   what is int main()
+    function main begins program execution 
+   what is {
+    {, begins the body of every function
+    what is //printf
+    The printf function can print Welcome to C!
+    what is preprocessor
+     preprocessor directives, which
+indicate that certain manipulations are to be performed on the program before compilation
+what is program execution
+Program execution continues with the next statement.
+what is return0;
+return 0; /* indicate that program ended successfully 
+what is }
+ }  end function main 
